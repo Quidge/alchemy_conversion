6c35e48cb4c9cc7d924cb4d546dffc511714d5ce
Quidge <trydemirgian@gmail.com>
2018-02-10 19:06:56 -0500

    Copied most of the boilerplate. From here I actually to read some SQLAlchemy docs.

---
+++ alch_ver/venv/application.py
@@ -0,0 +1,30 @@
+import os
+
+from time import gmtime, strftime
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import *
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
